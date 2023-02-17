cf1833b4bed828b7ef3f1eb80b358bbd86bc82cc
SalomeNderu <sallienderu@gmail.com>
2023-02-17 10:29:23 +0300

    django with loaded template plus images

--- nate_django/views.py
+++ nate_django/views.py
@@ -8,23 +8,30 @@ def home(request):
 def about(request):
     return render(request, "about.html")
 
+
 def contact(request):
     return render(request, "contact.html")
+
 
 def services(request):
     return render(request, "services.html")
 
+
 def about2(request):
     return render(request, "about2.html")
+
 
 def testimonials(request):
     return render(request, "testimonials.html")
 
+
 def contact2(request):
     return render(request, "contact2.html")
 
+
 def services2(request):
     return render(request, "services2.html")
+
 
 def index(request):
     return render(request, "index.html")
